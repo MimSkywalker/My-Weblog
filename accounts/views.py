38e649296c417d8ab9691cf2b7e8b3b5abf9e2b2
Mohammad <mdmdnezhad@gmail.com>
2026-06-29 18:06:55 +0330

    Config message and css of messages

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomLoginForm
 from .models import User
+from django.contrib import messages
 
 
 class SignUpView(CreateView):
@@ -11,6 +12,13 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "حساب کاربری شما با موفقیت ساخته شد. اکنون وارد شوید.")
+        return response
 
 
 
--- static/home/js/home.js
+++ static/home/js/home.js
@@ -100,3 +100,55 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cards = document.querySelectorAll(".msg-card");
+
+    cards.forEach(card => {
+
+        // دکمه X برای بستن دستی
+        const closeBtn = card.querySelector(".msg-close");
+
+        if (closeBtn) {
+            closeBtn.addEventListener("click", () => {
+                card.classList.add("fade-out");
+                setTimeout(() => card.remove(), 500);
+            });
+        }
+
+        // بستن خودکار بعد از 5 ثانیه
+        setTimeout(() => {
+            card.classList.add("fade-out");
+            setTimeout(() => card.remove(), 500);
+        }, 5000);
+
+    });
+
+});
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cards = document.querySelectorAll(".msg-card");
+
+    cards.forEach(card => {
+
+        // دکمه X برای بستن دستی
+        const closeBtn = card.querySelector(".msg-close");
+
+        if (closeBtn) {
+            closeBtn.addEventListener("click", () => {
+                card.classList.add("fade-out");
+                setTimeout(() => card.remove(), 500);
+            });
+        }
+
+        // بستن خودکار بعد از 5 ثانیه
+        setTimeout(() => {
+            card.classList.add("fade-out");
+            setTimeout(() => card.remove(), 500);
+        }, 5000);
+
+    });
+
+});
